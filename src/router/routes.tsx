c7527eaadb96696126626c2e1be3fb32f57db1a8
Alexey Merkulov laptop hua <almerkylov@mail.ru>
2025-08-04 01:30:19 +0300

    u

--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -4,24 +4,29 @@ import { Questions } from '../components/questions'
 import { Welcome } from '../components/welcome'
 import { Instruction } from '../components/instruction'
 
-export const routes = createBrowserRouter([
+export const routes = createBrowserRouter(
+    [
+        {
+            path: '/',
+            element: <Welcome />,
+        },
+        {
+            path: '/instruction',
+            element: <Instruction />,
+        },
+        {
+            path: '/question',
+            element: <QuestionLayout />,
+            errorElement: 'Такой страницы не существует',
+            children: [
+                {
+                    path: ':id',
+                    element: <Questions />,
+                },
+            ],
+        },
+    ],
     {
-        path: '/',
-        element: <Welcome />,
-    },
-    {
-        path: '/instruction',
-        element: <Instruction />,
-    },
-    {
-        path: '/question',
-        element: <QuestionLayout />,
-        errorElement: 'Такой страницы не существует',
-        children: [
-            {
-                path: ':id',
-                element: <Questions />,
-            },
-        ],
-    },
-])
+        basename: '/natalya',
+    }
+)
